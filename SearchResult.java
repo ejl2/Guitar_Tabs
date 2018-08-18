7a24cbe5cff920b8d4bdab8dc728130c486a56f2
e2929 <30487487+e2929@users.noreply.github.com>
2018-08-18 10:54:10 -0400

    Outline searchresult object

---
+++ SearchResult.java
@@ -0,0 +1,17 @@
+
+
+public class SearchResult {
+
+private String name;
+private String type;
+private String rating;
+private String popularity;
+
+    public SearchResult(String html) {
+      name = null;
+      type = null;
+      rating = null;
+      popularity = null;
+      
+    }
+}
