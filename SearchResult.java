bc406404b8003d1c809843f65d2616758ab3968f
e2929 <30487487+e2929@users.noreply.github.com>
2018-08-20 22:38:35 -0400

    open new browser with desired tab

--- SearchResult.java
+++ SearchResult.java
@@ -1,17 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 
 
 public class SearchResult {
 
 private String name;
+private int popularity;
 private String type;
-private String rating;
-private String popularity;
+private boolean isTab = false;
+private String link;
 
-    public SearchResult(String html) {
-      name = null;
-      type = null;
-      rating = null;
-      popularity = null;
-      
+    public SearchResult(WebElement html) {
+
+      link = html.findElement(By.cssSelector("a[class='link-primary _1kcZ5']")).getAttribute("href");
+
+      name = html.findElement(By.cssSelector("a[class='link-primary _1kcZ5']")).getText();
+      try {
+        popularity = Integer.parseInt(html.findElement(By.cssSelector("div[class='_1RJlF _3IBgH']")).getText());
+      } catch(NumberFormatException e) {
+        popularity = 0;
+      }
+      type = html.findElement(By.cssSelector("div[class='_1RJlF _2fyaZ']")).getText();
+
+      if (type.equalsIgnoreCase("tab")) {
+        isTab = true;
+      }
+    }
+
+    public String toString() {
+      return "\n Name: " + name + " " +
+      "\n Type: " + type + " " +
+      "\n Popularity: " + popularity +
+      "\n Address: " + link + "\n";
+    }
+
+    public boolean isTab() {
+      return isTab;
+    }
+
+    public String getLink() {
+      return link;
     }
 }
--- SeleniumTest.java
+++ SeleniumTest.java
@@ -3,12 +3,14 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebDriver.Timeouts.*;
 
 
 public class SeleniumTest {
@@ -20,17 +22,26 @@ public class SeleniumTest {
     chromeOptions.addArguments("window-size=1200x600");
 
     WebDriver driver = new ChromeDriver(chromeOptions);
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.get("https://www.ultimate-guitar.com/search.php?search_type=title&value=weird%20fishes");
 
     //System.out.println(driver.getPageSource());
 
     List<WebElement> hits = driver.findElements(By.cssSelector("div[class='_1iQi2']"));
-    Vector<WebElement> searchResults = new Vector<WebElement>();
+
+    Vector<SearchResult> searchResults = new Vector<SearchResult>();
     System.out.println(hits.size());
-    for (int i = 0; i < hits.size(); i++) {
-      System.out.println(hits.get(i));
-      System.out.println(hits.get(i).getText());
-      searchResults.add(hits.get(i)); 
+    for (int i = 1; i < hits.size(); i++) {
+      //System.out.println(hits.get(i));
+      searchResults.add(new SearchResult(hits.get(i)));
     }
+
+    System.out.println(searchResults);
+    driver.close();
+
+
+    driver = new ChromeDriver();
+    driver.get(searchResults.get(1).getLink());
+    //driver.close();
   }
 }
